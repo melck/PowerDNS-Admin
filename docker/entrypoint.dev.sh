7b15fc5e1e5a33d86e395c0b15043a81cd600ef0
Melchior NOGUES <m3lchior@gmail.com>
2022-09-14 11:01:18 +0200

    chore: update dev workflow

---
+++ docker/entrypoint.dev.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+build_assets() {
+  yarn install --pure-lockfile --production
+  flask assets build
+}
+
+action=$1
+shift 1
+
+case "$action" in
+dev)
+  build_assets
+  flask db upgrade
+  exec flask run --debugger --host 0.0.0.0
+  ;;
+*)
+  exec "$action" "$@"
+  ;;
+esac
